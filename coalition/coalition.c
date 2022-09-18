b85e016ba7a95d4b4e2f39cf18b0b5ea51d94c4d
dantremb <dany.tremblay@gmail.com>
2022-09-17 23:17:57 -0400

    17 September 23:17:57

--- coalition/coalition.c
+++ coalition/coalition.c
@@ -6,31 +6,34 @@
 /*   By: dantremb <dantremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/15 22:08:15 by dantremb          #+#    #+#             */
-/*   Updated: 2022/09/16 00:27:41 by dantremb         ###   ########.fr       */
+/*   Updated: 2022/09/17 23:14:28 by dantremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/coalition.h"
-
-void	choose_color(char *user)
-{
-	FILE *file;
-	char buffer[10];
-	(void)user;
-	
-	file = fopen("user.txt", "r");
-	if (file == NULL)
-		printf("user.txt not found\n");
-	while (fgets(buffer, sizeof(buffer), file))
-	{
-		printf("%s", buffer);
-	}
-}
+#include <string.h> 
+#include <stdio.h>
+#include <stdlib.h>
 
 int main (int ac, char **av)
 {
-	if (ac != 2)
-		printf("Error!\n");
+	FILE *fd;
+	char buffer[100];
+	
+	if (ac == 2)
+	{
+		fd = fopen("user.txt", "r");
+		if (fd == NULL) {
+			printf("42Choixpeau: user.txt not found\n");
+			exit(1);
+		}
+		while (fgets(buffer, 100, fd) != NULL) {
+			if (strncmp(buffer, av[1], strlen(av[1])) == 0 && buffer[strlen(av[1])] == ' ') {
+				printf("%s", buffer + strlen(av[1]) + 1);
+				exit(0);
+			}
+		}
+		printf("42Choixpeau: No match found: [%s]\n", av[1]);
+	}
 	else
-		choose_color(av[1]);
+		printf("42Choixpeau: Usage: <./a.out [name]>\n");
 }
